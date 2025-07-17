8abb4fa9396c462640233c4769ca9469c8bc84c2
Mike <miguel.velez.dev@gmail.com>
2025-07-16 21:33:36 -0500

    Commit Final del Challenge LiterAlura

--- src/main/java/com/IronMike524/literalura/LiteraluraApplication.java
+++ src/main/java/com/IronMike524/literalura/LiteraluraApplication.java
@@ -1,13 +1,23 @@
 package com.IronMike524.literalura;
 
+import com.IronMike524.literalura.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private MenuService menuService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		menuService.mostrarMenu();
+	}
 }
